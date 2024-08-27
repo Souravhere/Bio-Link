024dab5a58e8616d912ce8ae4e70c2bade5745e9
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-27 19:16:35 +0530

    simple Cylinder Gemetry created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { MeshBasicMaterial } from "three";
 
 function App() {
-
   return (
     <>
-      This is the app.jsx
+      <div className="w-full h-screen bg-black">
+        <Canvas>
+          <OrbitControls/>
+          <ambientLight/>
+          <mesh>
+            <cylinderGeometry args={[3 ,3, 10, 10, 10, false]}/>
+            <meshBasicMaterial color="white " />
+          </mesh>
+        </Canvas>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+ 
