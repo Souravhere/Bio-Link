782d4ffd1e72220d6eabc8c95b45599ba3fd55cb
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-27 22:26:09 +0530

    sourace image file location updated to check the 3d render in vercel

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -5,7 +5,7 @@ import { MeshBasicMaterial } from "three";
 import * as THREE from 'three';
 function Scene() {
     // let tex = useTexture('./../SourceImg/bio-link-main.png');
-    let tex = useTexture('./../SourceImg/Frame-1.png');
+    let tex = useTexture('./SourceImg/Frame-1.png');
     tex.needUpdate = true;
     console.log(tex);
     
