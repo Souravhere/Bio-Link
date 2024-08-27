dc5301bdd73acd3b2de942106a6b3f4ec3233192
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-27 22:20:58 +0530

    Few more images added to check the quality of 3d modal

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -4,13 +4,14 @@ import { Canvas } from "@react-three/fiber";
 import { MeshBasicMaterial } from "three";
 import * as THREE from 'three';
 function Scene() {
-    let tex = useTexture('./../SourceImg/bio-link-main.png');
+    // let tex = useTexture('./../SourceImg/bio-link-main.png');
+    let tex = useTexture('./../SourceImg/Frame-1.png');
     tex.needUpdate = true;
     console.log(tex);
     
   return (
     <mesh>
-    <cylinderGeometry args={[2 ,2, 3, 100, 1000, true]}/>
+    <cylinderGeometry args={[2 ,2, 3, 500, 500, true]}/>
     <meshBasicMaterial map={tex} side={THREE.DoubleSide} />
     </mesh>
   )
