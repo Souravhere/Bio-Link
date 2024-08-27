b8af4da624134e60793660295b10741c0de0e856
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-27 22:36:23 +0530

    source img location are changed to debug the vercel error

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { OrbitControls, useTexture } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { MeshBasicMaterial } from "three";
 import * as THREE from 'three';
+
 function Scene() {
-    // let tex = useTexture('./../SourceImg/bio-link-main.png');
-    let tex = useTexture('./SourceImg/Frame-1.png');
-    tex.needUpdate = true;
-    console.log(tex);
-    
-  return (
-    <mesh>
-    <cylinderGeometry args={[2 ,2, 3, 500, 500, true]}/>
-    <meshBasicMaterial map={tex} side={THREE.DoubleSide} />
-    </mesh>
-  )
+    // Load the texture using the correct root path
+    let tex = useTexture('/SourceImg/Frame-1.png');
+
+    return (
+        <mesh>
+            <cylinderGeometry args={[2, 2, 3, 500, 500, true]} />
+            <meshBasicMaterial map={tex} side={THREE.DoubleSide} />
+        </mesh>
+    );
 }
 
-export default Scene
+export default Scene;
