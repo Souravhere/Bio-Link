92e71e2a5698168423213e91015575916e8b7661
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-28 18:58:00 +0530

    Few componest update and beta test

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,47 @@ import React from 'react';
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Scene from "./Components/Scene";
-import { BlurPass, Resizer, KernelSize, Resolution } from 'postprocessing';
 import { Bloom } from '@react-three/postprocessing';
-import { EffectComposer, ToneMapping } from '@react-three/postprocessing';
+import { EffectComposer } from '@react-three/postprocessing';
 
 function App() {
   return (
-    <div className="w-full h-screen bg-black">
-      <Canvas flat camera={{fov:30}}>
+    <div className="w-full bg-black md:h-96 lg:h-[600px] flex items-center justify-evenly">
+      <div className='w-1/2 md:h-96 lg:h-[600px] sm:-ml-[15%]'>
+      <Canvas flat camera={{fov: 40, position: [0, 0, 5] }} resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}>
         <ambientLight />
-        <OrbitControls />
+        <OrbitControls enablePan={false} enableZoom={false} />
         <Scene />
         <EffectComposer>
-        <Bloom
+          <Bloom
             mipmapBlur
             intensity={1}
             luminanceThreshold={2} 
             luminanceSmoothing={2}
           />
-          {/* <ToneMapping/> */}
         </EffectComposer>
       </Canvas>
+      </div>
+      <div>
+        <h1 className='text-white text-3xl font-semibold text-center my-3'>Projects</h1>
+        <div>
+          
+
+<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Read more
+        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+    </a>
+</div>
+
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -6,15 +6,15 @@ import { Group } from 'three/examples/jsm/libs/tween.module.js';
 
 function Scene() {
     // Load the texture using the correct root path
-    const tex = useTexture('./Frame-1.png');
+    const tex = useTexture('./Frame-2.png');
     let Scene = useRef(null)
     useFrame((state, delta) =>{
         Scene.current.rotation.y += delta * 0.5;
     });
     return (
-        <group rotation={[0, 1.3, 0.3]}>
+        <group rotation={[0.2, 1.3, -0.2]}>
             <mesh ref={Scene}>
-            <cylinderGeometry args={[1, 1, 1, 100, 100, true]} />
+            <cylinderGeometry args={[1, 1, 1.3, 100, 100, true]} />
             <meshBasicMaterial map={tex} transparent side={THREE.DoubleSide} />
         </mesh>
         </group>
